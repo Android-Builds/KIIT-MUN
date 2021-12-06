9cc07518bc4c4dcf6f3696206bf49418d20cd507
xayron <zxyron0@gmail.com>
2021-12-06 21:58:09 +0530

    Change launcher icon [pt2]

--- android/app/src/main/kotlin/com/kiit/mun/MainActivity.kt
+++ android/app/src/main/kotlin/com/kiit/mun/MainActivity.kt
@@ -1,6 +1,14 @@
 package com.kiit.mun
 
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+       super.onCreate(savedInstanceState)
+
+       // Handle the splash screen transition.
+       val splashScreen = installSplashScreen()
+    }
 }
